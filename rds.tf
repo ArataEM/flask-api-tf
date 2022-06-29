59eb66b7f249a85841ca40e43424df41db7dcc9f
Artem Usenko <artem.usenko42@gmail.com>
2022-06-29 18:48:24 +0300

    changed db instance type

--- rds.tf
+++ rds.tf
@@ -12,7 +12,7 @@ resource "aws_db_instance" "flask-api-db" {
   allocated_storage      = 10
   engine                 = "mysql"
   engine_version         = "8.0.28"
-  instance_class         = "db.t3.micro"
+  instance_class         = "db.t2.micro"
   db_subnet_group_name   = aws_db_subnet_group.flask-api-db.name
   vpc_security_group_ids = [aws_security_group.flask-api-db.id]
   #db_name                = "flaskapi"
